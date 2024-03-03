f5405243a05f6528442be3100949836d05a8c7dd
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2024-03-03 15:15:39 +0100

    created gitignore file

---
+++ models/user.js
@@ -0,0 +1,37 @@
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/routes.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const User = require('../models/user'); 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+
+
+router.post("/signup", async (req, res) => {
+  try {
+    // Hash the user's password
+    const hashedPassword = await bcrypt.hash(req.body.password, 8);
+    const user = new User({
+      ...req.body,
+      password: hashedPassword, // Use the hashed password
+    });
+    await user.save();
+
+    // Sign the JWT with the user's ID and your secret
+    const token = jwt.sign(
+      { _id: user._id.toString() },
+      process.env.JWT_SECRET
+    );
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Login route
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send("Unable to login");
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(400).send("Unable to login");
+    }
+
+    const token = jwt.sign(
+      { _id: user._id.toString() },
+      process.env.JWT_SECRET
+    );
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+
+router.post("/", async (req, res) => {
+  const { name, email, password, address, salary } = req.body;
+  const user = new User({
+    name,
+    email,
+    password,
+    address,
+    salary,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.status(201).send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+// Get all Users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+// Get a single User by id
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+// Update a User by id
+router.patch('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete a User by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+
+
+module.exports = router;
