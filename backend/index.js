2bbbbdce516f156a1d0ecf3371585181ee872af4
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2024-03-08 20:30:51 +0100

    re-ordered my index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const userRoutes = require("./routes/routes");
-const port = process.env.PORT || 4000;
 
-const app = express();
 dotenv.config();
-
+const app = express();
+const port = process.env.PORT || 4000;
+const db = process.env.MONGODB_URL;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://mrkiksycrm.cloud", 
+  })
+);
 
-
-
-// Connect to database
-const db = process.env.MONGODB_URL;
+// Connect to MongoDB
 mongoose
   .connect(db, {
     useNewUrlParser: true,
@@ -24,24 +25,15 @@ mongoose
   .then(() => console.log("Connected successfully to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
 
-
-// Configure CORS
-app.use(
-  cors({
-    origin: "http://mrkiksycrm.cloud",
-  })
-);
-
+// Root route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
-app.listen(port, () => {
-  console.log(`Server Started at ${port}`);
-});
-
-
+// User routes
 app.use("/api/users", userRoutes);
 
-
+// Start server
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
+});
