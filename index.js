6177c4656a299893d06b980b512e1db68e96775b
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2024-03-08 23:11:45 +0100

    recover

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const userRoutes = require("./routes/routes");
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://mrkiksycrm.cloud",
+  })
+);
+
+// Connect to database
+const db = process.env.MONGODB_URL;
+mongoose
+  .connect(db, {
+  })
+  .then(() => console.log("Connected successfully to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
+});
+
+app.use("/api/users", userRoutes);
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+ 
+});
+
+
+
+module.exports = mongoose.model("User", userSchema);
