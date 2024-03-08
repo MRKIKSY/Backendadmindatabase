fb60694e2382777d80f8f2ca4d3282d4b48cd8a4
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2024-03-08 20:37:13 +0100

    removed the connection topology

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,6 @@ app.use(
 // Connect to MongoDB
 mongoose
   .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected successfully to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
